3cc3b591a66cf9158d893e1706e8da13a97adc7e
Yassinesahyane89 <yassine.sahyane89@gmail.com>
2023-04-10 14:19:22 +0000

    change menu for dashboard

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Salahhusa9\Menu\Facades\Menu;
 
 class MenuServiceProvider extends ServiceProvider
 {
@@ -23,12 +24,6 @@ class MenuServiceProvider extends ServiceProvider
    */
   public function boot()
   {
-    $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
-    $verticalMenuData = json_decode($verticalMenuJson);
-    $horizontalMenuJson = file_get_contents(base_path('resources/menu/horizontalMenu.json'));
-    $horizontalMenuData = json_decode($horizontalMenuJson);
-
-    // Share all menuData to all the views
-    \View::share('menuData', [$verticalMenuData, $horizontalMenuData]);
+    Menu::add('Home', 'home', 'tf-icons ti ti-smart-home');
   }
 }
